17665e6ba24be8702514ee8b7cbcd4c584960a6d
Brian Doan <briandoan71@yahoo.com>
2019-07-25 08:26:15 -0400

    database fix

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABSE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres'
 })
 
